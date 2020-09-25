c36f082654c47cad45d945ff64943705eb043dee
Rajiv Ramjit <blackmamba214@hotmail.com>
2020-09-25 10:04:48 -0400

    Adding files for github

---
+++ precache-manifest.7b4e0b3885662e4e2521833d455f93aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c72d3e4c9fefd040916656d2ab662d",
+    "url": "/monster-roldex/index.html"
+  },
+  {
+    "revision": "14cd796150faf1b0ac96",
+    "url": "/monster-roldex/static/css/main.bb581bad.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monster-roldex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roldex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14cd796150faf1b0ac96",
+    "url": "/monster-roldex/static/js/main.d5f000d1.chunk.js"
+  },
+  {
+    "revision": "afcb4a477b21bd05ab13",
+    "url": "/monster-roldex/static/js/runtime-main.ec194bd4.js"
+  }
+]);
